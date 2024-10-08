8ada4c41a84d1e88ba4460d1be51b26c5578f809
dimaggessi <disomatt@hotmail.com>
2024-10-08 19:12:38 -0300

    add: Specification Pattern

--- src/GeekStore.Domain/Interfaces/IGenericRepository.cs
+++ src/GeekStore.Domain/Interfaces/IGenericRepository.cs
@@ -1,9 +1,16 @@
+using System.Reflection.Metadata;
 using GeekStore.Domain.Entities;
 
 namespace GeekStore.Domain.Interfaces
 {
     public interface IGenericRepository<T> where T : BaseEntity
     {
+        Task<T?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+        Task<IReadOnlyList<T>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<T?> GetEntityWithSpecification(ISpecification<T> specification,
+            CancellationToken cancellationToken = default);
+        Task<IReadOnlyList<T>> GetAllWithSpecificationAsync(ISpecification<T> specification,
+            CancellationToken cancellationToken = default);
         void Add(T entity);
         void Update(T entity);
         void Remove(T entity);
--- src/GeekStore.Infrastructure/Persistence/Repositories/GenericRepository.cs
+++ src/GeekStore.Infrastructure/Persistence/Repositories/GenericRepository.cs
@@ -1,5 +1,6 @@
 using GeekStore.Domain.Entities;
 using GeekStore.Domain.Interfaces;
+using GeekStore.Infrastructure.Persistence.Specifications;
 using Microsoft.EntityFrameworkCore;
 
 namespace GeekStore.Infrastructure.Persistence.Repositories
@@ -10,6 +11,26 @@ namespace GeekStore.Infrastructure.Persistence.Repositories
         public GenericRepository(GeekStoreDbContext context)
         {
             _context = context;
+        }
+
+        public async Task<T?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            return await _context.Set<T>().FindAsync(id, cancellationToken);
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync(CancellationToken cancellationToken = default)
+        {
+            return await _context.Set<T>().ToListAsync(cancellationToken);
+        }
+
+        public async Task<T?> GetEntityWithSpecification(ISpecification<T> specification, CancellationToken cancellationToken = default)
+        {
+            return await ApplySpecification(specification).FirstOrDefaultAsync(cancellationToken);
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllWithSpecificationAsync(ISpecification<T> specification, CancellationToken cancellationToken = default)
+        {
+            return await ApplySpecification(specification).ToListAsync(cancellationToken);
         }
 
         public void Add(T entity)
@@ -31,6 +52,12 @@ namespace GeekStore.Infrastructure.Persistence.Repositories
         public async Task<bool> ExistsAsync(int id, CancellationToken cancellationToken)
         {
             return await _context.Set<T>().CountAsync(x => x.Id == id, cancellationToken) > 0;
+        }
+
+        private IQueryable<T> ApplySpecification(ISpecification<T> specification)
+        {
+            IQueryable<T> query = _context.Set<T>().AsQueryable();
+            return SpecificationEvaluator<T>.GetQuery(query, specification);
         }
     }
 }
